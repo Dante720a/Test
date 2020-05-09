10dc836137c7a178378c5f9777663a7baa077b40
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-09 21:02:01 +0430

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 'TelegramProductionURL': 'https://mzytbot.herokuapp.com/', // do not forget trailing "/" !!
+ 'TelegramToken': '<1234943782:AAHt3JTZCwTokhlUb3-w-8LfrrGmzKnDKH0>', // Telegram token
+ 'googleSheetKey': '<2PACX-1vQ1m4EJm_YR8vtwA62qTcf_RKq3b5Uny2H798xD5JTyHwBxPlI3nWW8gkxpxp8fZqLTWdVZsrNPOdsK>', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ "confTimeZone": "Europe/Berlin" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
+}
