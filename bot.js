de55031de7e2f91a0be936aaacb4c32426880e18
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-10 11:37:42 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,7 +209,7 @@ bot.onText(/(.+)$/, function (msg, match) {
                         formattedAnswer += "\n";
                         
                     itemsFound++;
-                    formattedAnswer += '\u1f539' + item.content.$t; // add item content, '\u27a1' is the arrow emoji
+                    formattedAnswer += item.content.$t; // add item content, '\u27a1' is the arrow emoji
                 }
                 else if (currentHours == itemTime) // else collect items for the current hour
                 {
@@ -218,7 +218,7 @@ bot.onText(/(.+)$/, function (msg, match) {
                     else 
                         currentAnswer += "\n"; 
                         
-                    currentAnswer += '\u1f539' + item.content.$t; // get item content, '\u27a1' is the arrow emoji
+                    currentAnswer += item.content.$t; // get item content, '\u27a1' is the arrow emoji
                 }
                 
                 // else doing nothing
@@ -319,7 +319,7 @@ bot.onText(/(.+)$/, function (msg, match) {
                         formattedAnswer += "\n";
                         
                     itemsFound++;
-                    formattedAnswer += '\u1F4B0' + item.content.$t; // add item content, '\u27a1' is the arrow emoji
+                    formattedAnswer += item.content.$t; // add item content, '\u27a1' is the arrow emoji
                 }
                 else if (currentHours == itemTime) // else collect items for the current hour
                 {
@@ -328,7 +328,7 @@ bot.onText(/(.+)$/, function (msg, match) {
                     else 
                         currentAnswer += "\n"; 
                         
-                    currentAnswer += '\u1F4B0' + item.content.$t; // get item content, '\u27a1' is the arrow emoji
+                    currentAnswer += item.content.$t; // get item content, '\u27a1' is the arrow emoji
                 }
                 
                 // else doing nothing
