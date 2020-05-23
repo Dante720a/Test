5764d2814b42bed8499a79e80a499c8f3f6ac8fc
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-23 10:22:19 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
  'TelegramProductionURL': 'https://mzytbot.herokuapp.com/', // do not forget trailing "/" !!
  'TelegramToken': '1032627408:AAEIX2vf_EJP7SW19xPWhj2Rkq3g_s-LL3w', // Telegram token
- 'googleSheetKey': '1VgNwmHdZXpeSPZACvA_2CxS0TY-sv2hfffTrBlOy838', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
+ 'googleSheetKey': '1GacQyM3PEIyaRGzoUIMmcHwN7eea1EsTjQ-0i8tRA5o', // the key of the google sheet (should be public!), extract key from the google sheet doc publich url
  "confTimeZone": "Asia/Tehran" // time zone of the conference in the form like "Europe/Berlin", see http://momentjs.com/timezone/
 }
