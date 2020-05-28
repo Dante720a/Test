b3630bda8b451c976b95d02da78e6cd87ba9615b
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-28 11:00:46 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,9 +110,9 @@ bot.onText(/(.+)$/, function (msg, match) {
         // send message telegram finally
 	
 	var MMSG1 = formattedAnswer;
-	    
+	var MMSG1F = MMSG1.substring(6, 15);
 	setTimeout(() => { 
-		bot.sendMessage(msg.chat.id, MMSG1).then(function () {
+		bot.sendMessage(msg.chat.id, MMSG1F).then(function () {
         	});
 	}, 500);
 
