db8f9e17ab0a08d9f9152b2add1ab8ab3ef8317e
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-28 10:27:19 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,8 +22,6 @@ console.log('secon-bot server started...');
 // Make sure it is public or set to Anyone with link can view 
 // "od6" is the fist worksheet in the spreadsheet
 var WrkSheet01 = "https://spreadsheets.google.com/feeds/list/" + config.googleSheetKey + "/1/public/values?alt=json";
-var WrkSheet02 = "https://spreadsheets.google.com/feeds/list/" + config.googleSheetKey + "/2/public/values?alt=json";
-var WrkSheet03 = "https://spreadsheets.google.com/feeds/list/" + config.googleSheetKey + "/3/public/values?alt=json";
 
 var moment = require('moment-timezone');
 
@@ -42,12 +40,7 @@ bot.onText(/(.+)$/, function (msg, match) {
         
 		
 		
-		if (error || response.statusCode != 200) {
-            console.log('Error: '+error); // Show the error
-            console.log('Status code: ' + response.statusCode); // Show the error
-            return;
-        }
-        
+    
         var parsed = JSON.parse(body);
         var targetTime = NaN;   
 	   if (!isNaN(keywords))   // isNaN returns false if the value is number
@@ -101,6 +94,9 @@ bot.onText(/(.+)$/, function (msg, match) {
                     itemsFound++;
                     formattedAnswer += item.content.$t; // add item content, '\u27a1' is the arrow emoji
                 }
+				
+				
+				
                 else if (currentHours == itemTime) // else collect items for the current hour
                 {
                     if (currentAnswer == '')
@@ -115,29 +111,6 @@ bot.onText(/(.+)$/, function (msg, match) {
         });
         
        
-
-
-
-	   
-		// if no items were found for the given time 
-        if (itemsFound == 0)
-        {
-            if (targetTime<0 || targetTime>24)
-               // formattedAnswer = "اطلاعاتی پیدا نشد" + ".\n"+ "لطفاً کد رابصورت صحیح وارد نمایید" + ".\n";
-		    formattedAnswer = "";
-            else 
-                // formattedAnswer = " اطلاعاتی پیدا نشد( " + targetTime+ " ч)";
-		    formattedAnswer = "";
-                
-            // output current answer
-            if (currentAnswer != '')
-            {
-                formattedAnswer += "Hi! As of " + currentHours + ":" + currentMinutes + " " + config.confTimeZone+ " these talks are going:\n";
-                formattedAnswer += currentAnswer;
-            }
-        }
-
- 
         // send message telegram finally
 	
 	var MMSG1 = formattedAnswer;   
@@ -152,239 +125,6 @@ bot.onText(/(.+)$/, function (msg, match) {
     });
 
 //End of Get Sheet1
-
-
-
-//Start of Get Sheet2
-
-    request(WrkSheet02, function (error, response, body) {
-        
-		
-		
-		if (error || response.statusCode != 200) {
-            console.log('Error: '+error); // Show the error
-            console.log('Status code: ' + response.statusCode); // Show the error
-            return;
-        }
-        
-        parsed = JSON.parse(body);
-        targetTime = NaN;   
-	   if (!isNaN(keywords))   // isNaN returns false if the value is number
-       	   {
-            try{
-                targetTime = parseInt(keywords, 10);
-            }
-            catch(e){
-                targetTime = NaN;
-            }
-        }
-        
-        if (isNaN(targetTime))
-            targetTime = -1;
-	    
-        formattedAnswer = "";  
-        
-        // debug purposes: echo from id: 
-        // formattedAnswer += "\nMsg.from.id=" + msg.from.id + "\n";
-    
-        currentHours = parseInt(moment().tz(config.confTimeZone).format('HH'),10);
-        currentMinutes = parseInt(moment().tz(config.confTimeZone).format('mm'),10);
-        // console.log("Current hours: " + currentHours);
-        currentAnswer = "";
-        
-        itemsFound = 0;
-        // sending answers
-        parsed.feed.entry.forEach(function(item){
-                // get the time(in hours) from the very first column
-                itemTime = NaN;
-                itemTitle = item.title.$t
-                try{
-                    itemTime = parseInt(itemTitle, 10);
-                }
-                catch(e)
-                {
-                    itemTime = NaN;
-                }
-                
-                if (
-                    (!isNaN(itemTime) && itemTime == targetTime) ||
-                    (isNaN(itemTime) && itemTitle.toLowerCase().trim() == keywords.toLowerCase().trim())
-                    )
-                {
-                    // add the line break if not the first answer
-                    if (itemsFound==0) 
-                        formattedAnswer += "" + "\n";
-                    else 
-                        formattedAnswer += "\n";
-                        
-                    itemsFound++;
-                    formattedAnswer += item.content.$t; // add item content, '\u27a1' is the arrow emoji
-                }
-                else if (currentHours == itemTime) // else collect items for the current hour
-                {
-                    if (currentAnswer == '')
-                        currentAnswer == 'Starting from ' + currentHours + " h the following talks are goinf:\n";
-                    else 
-                        currentAnswer += "\n"; 
-                        
-                    currentAnswer += item.content.$t; // get item content, '\u27a1' is the arrow emoji
-                }
-                
-                // else doing nothing
-        });
-        
-       
-
-
-
-	   
-		// if no items were found for the given time 
-        if (itemsFound == 0)
-        {
-            if (targetTime<0 || targetTime>24)
-                //formattedAnswer = "اطلاعاتی برای کد وارد شده پیدا نشد" + ".\n"+ "لطفاً کد رابصورت صحیح وارد نمایید" + ".\n";
-           formattedAnswer = "";
-		    else 
-                //formattedAnswer = "پیدا نشد ( " + targetTime+ " ч)";
-                formattedAnswer = "";
-            // output current answer
-            if (currentAnswer != '')
-            {
-                formattedAnswer += "Hi! As of " + currentHours + ":" + currentMinutes + " " + config.confTimeZone+ " these talks are going:\n";
-                formattedAnswer += currentAnswer;
-            }
-        }
- 
-
-        // send message telegram finally
-	formattedAnswer += "\n" + ".";
-	var MMSG2 = formattedAnswer; 
-	setTimeout(() => { 
-		bot.sendMessage(msg.chat.id, MMSG2).then(function () {
-        	});
-	}, 2000);
-
-
-	    
-    });
-
-//End of Get Sheet2
-	
-
-	
-//Start of Get Sheet3
-
-    request(WrkSheet03, function (error, response, body) {
-        
-		
-		
-		if (error || response.statusCode != 200) {
-            console.log('Error: '+error); // Show the error
-            console.log('Status code: ' + response.statusCode); // Show the error
-            return;
-        }
-        
-        parsed = JSON.parse(body);
-        targetTime = NaN;   
-	   if (!isNaN(keywords))   // isNaN returns false if the value is number
-       	   {
-            try{
-                targetTime = parseInt(keywords, 10);
-            }
-            catch(e){
-                targetTime = NaN;
-            }
-        }
-        
-        if (isNaN(targetTime))
-            targetTime = -1;
-        
-        formattedAnswer = "";  
-	    
-        // debug purposes: echo from id: 
-        // formattedAnswer += "\nMsg.from.id=" + msg.from.id + "\n";
-    
-        currentHours = parseInt(moment().tz(config.confTimeZone).format('HH'),10);
-        currentMinutes = parseInt(moment().tz(config.confTimeZone).format('mm'),10);
-        // console.log("Current hours: " + currentHours);
-        currentAnswer = "";
-        
-        itemsFound = 0;
-        // sending answers
-        parsed.feed.entry.forEach(function(item){
-                // get the time(in hours) from the very first column
-                itemTime = NaN;
-                itemTitle = item.title.$t
-                try{
-                    itemTime = parseInt(itemTitle, 10);
-                }
-                catch(e)
-                {
-                    itemTime = NaN;
-                }
-                
-                if (
-                    (!isNaN(itemTime) && itemTime == targetTime) ||
-                    (isNaN(itemTime) && itemTitle.toLowerCase().trim() == keywords.toLowerCase().trim())
-                    )
-                {
-                    // add the line break if not the first answer
-                    if (itemsFound==0) 
-                        formattedAnswer += "\n";
-                    else 
-                        formattedAnswer += "\n";
-                        
-                    itemsFound++;
-                    formattedAnswer += item.content.$t; // add item content, '\u27a1' is the arrow emoji
-                }
-                else if (currentHours == itemTime) // else collect items for the current hour
-                {
-                    if (currentAnswer == '')
-                        currentAnswer == 'Starting from ' + currentHours + " h the following talks are goinf:\n";
-                    else 
-                        currentAnswer += "\n"; 
-                        
-                    currentAnswer += item.content.$t; // get item content, '\u27a1' is the arrow emoji
-                }
-                
-                // else doing nothing
-        });
-        
-       
-
-
-
-	   
-		// if no items were found for the given time 
-        if (itemsFound == 0)
-        {
-            if (targetTime<0 || targetTime>24)
-                //formattedAnswer = "اطلاعاتی برای کد وارد شده پیدا نشد" + ".\n"+ "لطفاً کد قطعه رابصورت صحیح وارد نمایید" + ".\n";
-            formattedAnswer = "";
-		    else 
-                //formattedAnswer = "قبضی برای قطعه وارد شده پیدا نشد ( " + targetTime+ " ч)";
-                formattedAnswer = "";
-            // output current answer
-            if (currentAnswer != '')
-            {
-                formattedAnswer += "Hi! As of " + currentHours + ":" + currentMinutes + " " + config.confTimeZone+ " these talks are going:\n";
-                formattedAnswer += currentAnswer;
-            }
-        }
- 
-
-        // send message telegram finally
-	formattedAnswer += "\n" + ".";
-        var MMSG3 = formattedAnswer; 
-	setTimeout(() => { 
-		bot.sendMessage(msg.chat.id, MMSG3).then(function () {
-        	});
-	}, 3000);
- 
-    });
-
-//End of Get Sheet3
-
 
 	
 });
