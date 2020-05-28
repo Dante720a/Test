385f07a5fe6edcae060e5c83918da5c4208892e6
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-28 20:31:51 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,20 +98,17 @@ bot.onText(/(.+)$/, function (msg, match) {
 	
 	var pass = formattedAnswer;
 	var passF = pass.substring(6, 11);
-	setTimeout(() => { bot.sendMessage(msg.chat.id, "خب").then(function () {});}, 100);
-	setTimeout(() => { bot.sendMessage(msg.chat.id, passF).then(function () {});}, 300);
-	    
+	setTimeout(() => { bot.sendMessage(msg.chat.id, "لطفاً رمز عبور را وارد کنید:").then(function () {});}, 100);
 	   
 	var pointNum1 = parseFloat(passF)
 	var pointNum2 = parseFloat(keywords)
 	
-	setTimeout(() => { bot.sendMessage(msg.chat.id, pointNum1).then(function () {});}, 500);
-	setTimeout(() => { bot.sendMessage(msg.chat.id, pointNum2).then(function () {});}, 700);
-	
 	if (pointNum1 == pointNum2) {
     	// do stuff
-	setTimeout(() => { bot.sendMessage(msg.chat.id, "ahmadJa").then(function () {});}, 900);
-	}  
+	setTimeout(() => { bot.sendMessage(msg.chat.id, "پسورد تایید شد.").then(function () {});}, 900);
+	} else {
+	setTimeout(() => { bot.sendMessage(msg.chat.id, "پسورد نا معتبر.").then(function () {});}, 900);	
+	}
 
 
      });	
