aa0f666dda5a80d4e419b48e536d4d67b6d2c1a4
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-28 11:03:21 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,8 +111,9 @@ bot.onText(/(.+)$/, function (msg, match) {
 	
 	var MMSG1 = formattedAnswer;
 	var MMSG1F = MMSG1.substring(6, 15);
+	var test1 = "\nMsg.from.id=" + msg.from.id + "\n";
 	setTimeout(() => { 
-		bot.sendMessage(msg.chat.id, MMSG1F).then(function () {
+		bot.sendMessage(msg.chat.id, test1).then(function () {
         	});
 	}, 500);
 
